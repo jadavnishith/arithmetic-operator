f3db1fbd2adc1ad237b93230e091db746f911827
jadavnishith <mrnick076@gmail.com>
2023-12-25 15:58:51 +0530

    arithmetic operator

---
+++ index.c
@@ -0,0 +1,62 @@
+
+//arithmetic operator
+
+#include <stdio.h>
+
+int main()
+{
+    //addition
+    
+    int a=50;
+    int b=10;
+    int c=a+b;
+    printf("addition is.... ");
+    printf("%d\n\n",c);
+    
+    //subtraction
+    
+    int s=50;
+    int h=10;
+    int k=s-h;
+    printf("subtraction is.... ");
+    printf("%d\n\n",k);
+    
+    //multipliction
+    
+    int m=50;
+    int n=10;
+    int o=m*n;
+    printf("multipliction is.... ");
+    printf("%d\n\n",o);
+    
+    //division
+    
+    int d=50;
+    int i=10;
+    int v=d/i;
+    printf("division is.... ");
+    printf("%d\n\n",v);
+    
+    //modules
+    
+    int x=50;
+    int y=10;
+    int z=x%y;
+    printf("module is.... ");
+    printf("%d\n\n",z);
+    
+    //increment
+    
+    int u=20;
+    u++;
+    printf("increment is.... ");
+    printf("%d\n\n",u);
+    
+    //decrement
+    
+    int l=20;
+    l--;
+    printf("decrement is.... ");
+    printf("%d",l);
+    return 0;
+}
